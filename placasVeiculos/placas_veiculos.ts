0346dd883a6e8a85d643a074cf3b38464287970f
Emanoel Silva <emanoelsilva.jua@gmail.com>
2024-08-13 13:15:44 -0300

    Rota do relatorio

--- placasVeiculos/placas_veiculos.ts
+++ placasVeiculos/placas_veiculos.ts
@@ -44,18 +44,78 @@ rotas_placas.get("/consulta/:placa", async (req: Request, res: Response) => {
   }
 });
 
-// get com os relatorios
+// rotas_placas.get(
+//   "/relatorio/cidade/:cidade",
+//   async (req: Request, res: Response) => {
+//     const doc = require("fs");
+//     let Document_PDF = "pdfkit";
+
+//     // informações de número da placa, cidade, data e hora dos registros com uma determinada cidade passada no parâmetro
+//     const cidadeFiltrada = req.params.cidade;
+
+//     try {
+//       const db = await connectToDatabase();
+//       console.log("Cidade filtrada", cidadeFiltrada);
+
+//       const placaData = await db
+//         .collection("placas")
+//         .findOne({ cidade: cidadeFiltrada });
+
+//       if (placaData) {
+//         return res.json(placaData);
+//       } else {
+//         return res.status(404).send({ message: "Placa não encontrada" });
+//       }
+//     } catch (err) {
+//       res.status(500).json({ message: `Erro ao consultar relatório: ${err}` });
+//     }
+//   }
+// );
+
 rotas_placas.get(
   "/relatorio/cidade/:cidade",
   async (req: Request, res: Response) => {
-    // informações de número da placa, cidade, data e hora dos registros com uma determinada cidade passada no parâmetro
-    const cidade = req.params.cidade;
+    const cidadeFiltrada = req.params.cidade;
+    const fs = require("fs");
+    var PDFDocument = require("pdfkit");
 
     try {
       const db = await connectToDatabase();
-      // const placaData = await db
-      //   .collection("placas")
-      //   .findOne({ cidade: cidade, data: { $gte: dataFormatada } });
+      console.log("Cidade filtrada", cidadeFiltrada);
+
+      const placaData = await db
+        .collection("placas")
+        .findOne({ cidade: cidadeFiltrada });
+
+      if (placaData) {
+        const doc = new PDFDocument();
+
+        // Configura o cabeçalho da resposta HTTP para enviar um PDF
+        res.setHeader(
+          "Content-disposition",
+          `attachment; filename=relatorio_${cidadeFiltrada}.pdf`
+        );
+        res.setHeader("Content-type", "application/pdf");
+
+        // Criar o conteúdo do PDF
+        doc.fontSize(25).text("Relatório de Placas", { align: "center" });
+        doc.moveDown();
+        doc.fontSize(16).text(`Cidade: ${cidadeFiltrada}`);
+        doc.moveDown();
+
+        // Adiciona informações da placa ao PDF
+        doc.fontSize(14).text(`Número da Placa: ${placaData.num_placa}`);
+        doc.text(`Data: ${placaData.data_registro}`);
+        doc.text(`Hora: ${placaData.horario_registro}`);
+        doc.text(`Foto: ${placaData.foto}`);
+        // Continue adicionando mais informações conforme necessário
+
+        // Finaliza o documento e envia como resposta
+        doc.pipe(res);
+        doc.end();
+      } else {
+        return res.status(404).send({ message: "Placa não encontrada" });
+      }
     } catch (err) {
       res.status(500).json({ message: `Erro ao consultar relatório: ${err}` });
     }
@@ -63,3 +123,20 @@ rotas_placas.get(
 );
 
 export default rotas_placas;
+
+// const fs = require("fs");
+// let relatorio_PDF = require("pdfkit");
+
+// let doc = new relatorio_PDF();
+// doc.fontSize(20).text('Relatório de Placa', 100, 100);
+// doc.fontSize(15).text('Placa: ' + placaFilter, 100,120);
+// doc.save('relatorio.pdf');
+
+// fs.readFile('relatorio.pdf', (err, data) => {
+//   if (err) {
+//     res.status(500).json({ message: `Erro ao gerar relatório ${err}`
+//     });
+//   } else {
+//     res.download('relatorio.pdf', 'relatorio.pdf');
+//   }
+// });
